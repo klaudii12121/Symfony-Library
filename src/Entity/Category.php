27b2e83c4bfe287c2a8ab07725669a8889162840
root <19_kowalczyk@host.com>
2021-05-27 12:12:12 +0000

    27.05 14:12

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -9,6 +9,8 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Class Category.
